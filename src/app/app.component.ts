55ea48d4d471a5c4d68cf99b48624bb11ecf5e24
Camilo Mejia Monsalve <mejiamonsalve.camilo@gmail.com>
2025-12-09 11:33:47 -0500

    angular + angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatSlideToggleModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatInputModule,
+    MatFormFieldModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true
 })
 export class AppComponent {
+
+  constructor(
+    private iconReg: MatIconRegistry
+  ) {
+    this.iconReg.setDefaultFontSetClass('material-symbols-outlined')
+  }
+
   title = 'WebAppVitalFit';
+
+  toggleTheme() {
+    document.body.classList.toggle('dark-theme');
+  }
 }
